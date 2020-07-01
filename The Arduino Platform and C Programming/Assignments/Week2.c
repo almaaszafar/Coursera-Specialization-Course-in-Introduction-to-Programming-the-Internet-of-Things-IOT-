cf078abc8878e554deb47ca5763cfd96fe9790a9
Almaas Zafar <67468269+almaaszafar@users.noreply.github.com>
2020-07-01 20:20:11 +0530

    Create Week2.c

---
+++ The Arduino Platform and C Programming/Assignments/Week2.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main() 
+{ 
+ int i, n, a = 0, b = 1, c; 
+ printf("Fibonacci Series: "); 
+ for (i = 1; i <= 6; ++i) 
+ { printf("%d, ", t1); 
+ c = a + b; a = b; 
+ b = c; } 
+}
